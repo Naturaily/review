149f81a4ca89a78617951a0d9d9644379cc32ce7
Jacek Hiszpanski <j.hiszpanski@gmail.com>
2015-07-01 21:00:51 +0200

    Add bitbucket omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, AppConfig.github.appkey, AppConfig.github.secret
+  provider :bitbucket, AppConfig.bitbucket.appkey, AppConfig.bitbucket.secret
   provider :gitlab, AppConfig.gitlab.appkey, AppConfig.gitlab.secret, client_options: {
     site: ENV['GITLAB_SITE']
   }
